bb2b77131141e141b22b381f5dc4538f6c0a3e86
yeochinyi <yeochinyi@gmail.com[user]>
2014-09-24 18:06:59 +0800

    move to js proto

--- app/app.js
+++ app/app.js
@@ -12,6 +12,202 @@ String.prototype.toUnderscore = function(){
   return this.replace(/([A-Z])/g, function($1){return "_"+$1.toLowerCase();});
 };
 
+String.prototype.contains = function(text){
+  return this.indexOf(text) != -1;
+};
+
+String.prototype.extracts = function(textArray){
+  for(var i in textArray){
+    if(this.contains(textArray[i])) return textArray[i];
+  }
+  return undefined;
+};
+
+
+function DTable (){
+    /** Column standards “Using Convention Over Config"
+      ===================================================
+      id                  : each table will have one as primary key
+      <ref table>_refid      : map to another table
+      <named>-<ref table>_refid : 'named' ref table. this maps to a ref table but table header will show '<Colname> <ref table>'s col> on column header
+      *_[date,email,month,num,time,url,week,bool] : show the respective angular widget
+      Everything else will show a typeahead input type='text'
+    **/
+    // ref tablename vs map value i.e {'static_table_1':{ id:{col1:value1, col2:value2, ...}, ... }...}
+    this.mapOfData= {}; 
+    // ref tablename vs header data i.e {'static_table_1':[{name:col1, hide:false, ...}, {name:col2, hide:false, ...} ...]...}
+    /** Headers will contain the following info
+      id :  this will be the db col name to map back to the data key
+      hide : true -> hidden
+      sort : nil -> not sorted , true -> asc, false -> desc
+      type : extracted from the <colname>-[date,email,month,num,time,url,week,bool,refid]
+      ref : if != null, this header is a ref table id
+      ref_col : if has ref, this will be ref table's col to be display
+      named : text to show as 'named' ref
+
+      The order of the header obj will determine the display col order.
+    */
+    this.mapOfHeaders = {};  
+
+    // easier to call current data
+    this.currTable;
+
+}
+
+DTable.prototype.getRefData = function(header,value){
+  var refMap = this.getData(header.ref);
+  if(refMap == null) return value;
+  var refValue = refMap[value];
+  return refValue == null ? value : refValue[header.ref_col];
+}
+
+DTable.prototype.hasTable = function(table){
+  return this.mapOfData[table] != null;
+}
+
+DTable.prototype.addData = function(table,dataArray){
+
+  if(this.hasTable(table)) return;
+
+  var refMap = {};
+  for(var i = 0; i < dataArray.length; i++) {
+    var refId = dataArray[i].id;
+    refMap[refId] = dataArray[i];
+  };            
+  this.mapOfData[table] = refMap;
+  this.createHeaders(table,dataArray[0]);
+}
+
+
+/*** Tricky!
+  1. Get data for table 1 (which has ref to table 2)
+  2. Put data in cache map & extract headers (+info). At this pt, we don't know table2 col details yet.
+  3. With headers info, get data for table 2 (with ref to table 3). Now we have table2 col details.. don't get table 3
+  4. Continue from to 2 to create new table 1 headers with table 2 details.
+*/
+DTable.types = ['date','email','month','num','time','url','week','bool','refid'];
+
+DTable.prototype.createHeaders = function(table,dataObj){
+  var headers = [];
+  for(var id in dataObj){ //loop keys
+    if(!dataObj.hasOwnProperty(id)) continue;
+    var type = id.extracts(DTable.types);
+    var ref,named;
+    if(type == 'refid'){ // create additional cols
+      var i = id.indexOf('-');
+      var j = id.lastIndexOf('_');
+      // either named as 'named' table or take ref table name
+      named = i != -1 ? id.substring(0,i) : id.substring(0,j);
+      ref = i != -1 ? id.substring(i + 1,j) : named;
+    }
+    else{
+      named = id;
+    }
+    headers.push({
+      id:id, 
+      hide:false,
+      type:type,
+      named:named,
+      ref:ref,
+    });
+  }
+
+  this.mapOfHeaders[table] = headers;
+}
+
+DTable.prototype.expandHeaders = function(table){
+  var headers = this.mapOfHeaders[table];
+  for(var i in headers){
+    var newHeaders = [];
+    if(headers[i].ref != undefined && headers[i].ref_col == undefined){
+      var refTable = headers[i].ref;
+      var refHeaders = this.mapOfHeaders[refTable];
+      for(var j in refHeaders){
+        if(refHeaders[j].id == 'id') continue;
+        newHeaders.push({
+          id:headers[i].id,  //curr table id
+          hide:headers[i].hide,
+          type:'refid',
+          named:headers[i].named + '.' + refHeaders[j].id,
+          ref:refTable,
+          ref_col:refHeaders[j].id,
+        });        
+      }
+      if(newHeaders.length != 0){ // splice and insert array
+        headers.splice.apply(headers, [i,1].concat(newHeaders));
+      }
+    }
+  }
+}
+
+DTable.prototype.currRefTables = function(){
+  var r = [];
+  var headers = this.currHeaders();
+  for(var i in headers){
+    if(headers[i].id.contains('refid') &&  headers[i].ref != undefined){
+      r.push(headers[i].ref);
+    }
+  }
+  return r;
+}
+
+DTable.prototype.getLastId = function (){
+  var id = 0;
+
+  var data = this.currData()
+  for(var i in data){
+    var c = data[i]['id'];
+    if(c > id) id = c;
+  }      
+  return id;
+}
+
+DTable.prototype.rowContains = function (rowObj,text){
+  var headers = this.currHeaders();
+  for(var i in headers){ //Loop array
+    var h = headers[i]; 
+    if(!h.hide) continue;
+    var value = rowObj[h.id];              
+    var refData = dTable.getRefData(h,value);
+    if(refData.contains(text)) return true;    
+  }       
+  return false;
+}
+
+/*
+DTable.prototype.getDisplayData = function(data) {
+  var displayData = {};
+
+  var headers = this.currHeaders();
+  for(var j in headers){ //Loop array
+    var h = headers[j]; 
+    var value = data[h.name];              
+    var refValue = getRefValue(value,h.name);
+    displayData[h.name] = refValue;
+  }       
+  return displayData
+}    
+
+
+*/
+
+
+DTable.prototype.getData = function(table){
+    return this.mapOfData[table];
+}
+
+DTable.prototype.getHeader = function(table){
+    return this.mapOfHeaders[table];
+}
+
+DTable.prototype.currData = function(){
+    return this.getData(this.currTable);
+}
+
+DTable.prototype.currHeaders = function(){
+    return this.getHeader(this.currTable);
+}
+
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
@@ -25,100 +221,46 @@ factory('DataFactory',['$resource', function($resource) {
   });
   return p;
 }]).
-controller('DataTableCtrl', ['$scope','DataFactory','$timeout','$q','$modal',function($scope,DataFactory,$timeout,$q,$modal){
-    
-    // ref tablename vs map value i.e {'static_table_1':{ id:{col1:value1, col2:value2, ...}, ... }...}
-    var mapOfMap= {}; 
-    // ref tablename vs header data i.e {'static_table_1':[{name:col1, hide:false, ...}, {name:col2, hide:false, ...} ...]...}
-    var mapOfHeaders = {};
+controller('DataTableCtrl', ['$scope','DataFactory','$timeout','$q','$modal',function($scope,DataFactory,$timeout,$q,$modal){  
 
     $scope.tables = ['dynamic_table_2','static_table_1'];
 
-    var currentTable;
+    var dTable = new DTable();
 
     $scope.$watch('currentTable',function(newVal,oldVal){
 
       if(newVal == undefined) return;
-      currentTable = newVal;
+      dTable.currTable = newVal;
 
-      var data = DataFactory.query({table:currentTable});
+      var data = DataFactory.query({table:newVal});
       data.$promise.then(function(data){
         //Get Data
-        setMapFromQuery(data,currentTable);
+        dTable.addData(newVal,data);
 
         //wait for combined promises
         var promises = [];
         //Get Headers & all necessary static data
-
-        var headers = [];
-
-        angular.forEach(data[0], function(v, k){            
-          var hide = k.indexOf('desc') != -1 
-          headers.push({name:k, hide:hide});
-          var refTable = extractTableName(k);
-          if(refTable != null){
-            if(mapOfMap[refTable] == null){
-              var refData = DataFactory.query({table:refTable});
-              promises.push(refData.$promise.then(function(data){
-                setMapFromQuery(refData,refTable);
-              }));    
-            }
-          };
-        });
-
-        mapOfHeaders[currentTable] = headers;
+                
+        var tables = dTable.currRefTables();
+        for(var i in tables){
+          var table = tables[i];
+          var refData = DataFactory.query({table:table});
+          promises.push(refData.$promise.then(function(data){
+            dTable.addData(table,refData);
+          }));          
+        }
 
         //all promises resolved, time to massage current table data
         $q.all(promises).then(function(){
-          $scope.headers = currentHeader();        
-          $scope.mapOfMap = mapOfMap;
-          $scope.displayData = data;
+          dTable.expandHeaders(newVal);
+          $scope.dTable = dTable;
         });
       });      
-    });
+    });  
 
-
-
-    function setMapFromQuery(refData,refTable){
-      var refMap = {};
-      for(var i = 0; i < refData.length; i++) {
-        var refId = refData[i].id;
-        refMap[refId] = refData[i];
-      };            
-      //store into a map for ref
-      mapOfMap[refTable] = refMap;
-      mapOfMap[refTable + '_id'] = refMap; //For easy lookup using col id
-    }
-
-    function currentMap(){
-      return mapOfMap[currentTable];
-    }
-
-    function currentHeader(){
-      return mapOfHeaders[currentTable];
-    }    
-    
-    function getDisplayData(data) {
-      var displayData = {};
-
-      var headers = currentHeader();
-      for(var j in headers){
-        var h = headers[j]; 
-        var value = data[h];              
-        var refValue = getRefValue(value,h);
-        displayData[h] = refValue;
-      }       
-      return displayData
-    }    
-
-    function getRefValue(value,refTable){
-      var refMap = mapOfMap[refTable];
-      if(refMap == null) return value;
-      var refValue = refMap[value];
-      return refValue == null ? value : refValue.name;
-    }
-
-    $scope.getRefValue = getRefValue;
+    $scope.getRefValue = function (value,header){
+      return dTable.getRefData(header,value)
+    };
 
     $scope.sort = function(colName){
       $scope.orderProp = colName;
@@ -128,46 +270,31 @@ controller('DataTableCtrl', ['$scope','DataFactory','$timeout','$q','$modal',fun
       $scope.formObj = angular.copy(currentMap()[id]);
     };
 
-    function findDisplayData(obj, isReplace){
-      for(var i in $scope.displayData){
-        if($scope.displayData[i]['id'] == obj['id']){
-          if(isReplace){
-            $scope.displayData.splice(i,1,obj);
-          }
-          return;
-        }
-      }
-    };
-
     function add(obj){
-      currentMap()[obj['id']] = obj;
-      $scope.displayData.push(obj);
+      dTable.currData()[obj['id']] = obj;
     };
 
     function update(obj){
-      currentMap()[obj['id']] = obj;
-      findDisplayData(obj,true);
+      dTable.currData()[obj['id']] = obj;
     };
 
     function remove(obj){
-      currentMap()[obj['id']] = undefined;
-      findDisplayData(obj,false);
+      dTable.currData()[obj['id']] = undefined;
     };
 
-
-    function getLastId(){
-      var id = 0;
-
-      for(var i in currentMap()){
-        var c = currentMap()[i]['id'];
-        if(c > id) id = c;
-      }      
-      return id;
-    };
+    $scope.hide = function(colName){
+      var headers = currentHeader();
+      for(var i in headers){
+        if(headers[i].name == colName){
+          headers[i].hide = true;
+          return;
+        }
+      }
+    }
 
     $scope.clone = function(){      
       var formObj = $scope.formObj;
-      formObj['id'] = getLastId() + 1;
+      formObj['id'] = dTable.getLastId() + 1;
       add(formObj);
       modalInstance.close();
     };
@@ -200,13 +327,7 @@ controller('DataTableCtrl', ['$scope','DataFactory','$timeout','$q','$modal',fun
     $scope.filterRefValue = function(obj){
       var query = $scope.query;
       if(!query) return true;
-      var display = getDisplayData(obj);
-      
-      for(var k in display){
-        if(k == 'id') continue;
-        if(display.hasOwnProperty(k) && display[k].indexOf(query) != -1) return true
-      }
-      return false;
+      return dTable.rowContains(obj,query);
     };
 
     var modalInstance;
@@ -247,14 +368,15 @@ controller('TestCtrl', ['$scope','$http','DataFactory', 'SyncDataFactory','$q','
 }]).
 filter('cleanCol',function(){
   return function(input){    
-    var table = extractTableName(input);
-    var r =  table == null ? input : table;
-    return r.toUnderscore();
+    //var table = extractTableName(input);
+    //var r =  table == null ? input : table;
+    //return r.toUnderscore();
+    return input;
   }
 }).
 filter('convert',function(){
-  return function(value,idx,fn){
-    return fn(value,idx);      
+  return function(header,value,fn){
+    return fn(header,value);      
   }
 }).
 config(['$routeProvider', function($routeProvider) {
--- test/unit/test.js
+++ test/unit/test.js
@@ -1,6 +1,66 @@
 'use strict';
 
-/* jasmine specs for controllers go here */
+
+describe('DTable',function(){
+
+  beforeEach(function(){
+    this.addMatchers({
+      equals: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });  
+
+  it('test ops', function(){
+    var d = new DTable();
+
+    d.addData('table1',
+    [
+      {
+        'id': 1,
+        'name': 't11',
+        'table2_refid': 2,
+        'name1-table2_refid': 1,
+      },
+      {
+        'id': 2,
+        'name': 't12',
+        'table2_refid': 1,
+        'name1-table2_refid': 2,
+      }
+    ]);
+
+    d.addData('table2',
+    [
+      {
+        'id': 1,
+        'name': 't21',
+      },
+      {
+        'id': 2,
+        'name': 't22',
+      }
+    ]);    
+
+    d.currTable = 'table1';
+    d.expandHeaders('table1');
+
+    expect(d.hasTable('table1')).equals(true);
+    
+    expect(d.currHeaders()).equals(
+      [
+        { id : 'id', hide : false, named : 'id', }, 
+        { id : 'name', hide : false, named : 'name', }, 
+        { id : 'table2_refid', hide : false, type : 'refid', named : 'table2.name', ref : 'table2', rel_col : 'name' }, 
+        { id : 'name1-table2_refid', hide : false, type : 'refid', named : 'name1.name', ref : 'table2', rel_col : 'name' }
+      ]
+    );
+
+  });
+});
+
+
+
 describe('DataTableCtrl', function() {
 
   beforeEach(function(){
@@ -12,24 +72,24 @@ describe('DataTableCtrl', function() {
   });
 
   beforeEach(module('myApp'));
-    
+  
   var scope, ctrl, httpBackend;
   
   beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
     httpBackend = _$httpBackend_;      
-    //httpBackend.expectGET(/.*/).
     httpBackend.expectGET('sample/dynamic_table_2.json').respond([
       {
           "id": 1,
           "name": "dname1",
-          "static_table_1_id": 2
+          "static_table_1_refid": 2
       },
       {
           "id": 2,
           "name": "dname2",
-          "static_table_1_id": 1
+          "static_table_1_refid": 1
       }
     ]);
+    
     httpBackend.expectGET('sample/static_table_1.json').respond([
       {
           "id": 1,
@@ -49,25 +109,31 @@ describe('DataTableCtrl', function() {
     
   it('clone/update/delete', function() {
     //expect(scope.displayData).equals([]);
+    scope.currentTable =  'dynamic_table_2';
     httpBackend.flush();      
+    
+    /*
     expect(scope.displayData).equals([
       {
           "id": 1,
           "name": "dname1",
-          "static_table_1_id": 2
+          "static_table_1_refid": 2
       },
       {
           "id": 2,
           "name": "dname2",
-          "static_table_1_id": 1
+          "static_table_1_refid": 1
       }
     ]);
 
+    /*
     scope.formObj = {
           "id": 1,
           "name": "dname3",
           "static_table_1_id": 2
       };
+
+    
     scope.clone();
     expect(scope.displayData).equals([
       {
@@ -109,10 +175,7 @@ describe('DataTableCtrl', function() {
           "name": "dname3",
           "static_table_1_id": 2
       }
-    ]);
-
-
-
-
+    ]);*/
   });
+
 });
